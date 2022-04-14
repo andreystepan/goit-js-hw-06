aebf8ffca4178332bd98ea331ae49989582726b6
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-04-14 05:53:25 +0300

    task-2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const makeIngredients = arrayIngredients => {
+  return arrayIngredients.map(ingredient => {
+    const itemEl = document.createElement('li');
+    itemEl.classList.add('item');
+    itemEl.textContent = ingredient;
+
+
+    return itemEl;
+  })
+}
+
+const element = makeIngredients(ingredients);
+
+ingredientsEl.append(...element)
