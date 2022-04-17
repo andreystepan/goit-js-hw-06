9c24d7b78c096be39c5696827bec08fdfba2ef21
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-04-17 18:24:39 +0300

    task-06

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,7 @@ const inputText = document.querySelector('#validation-input');
 inputText.addEventListener('blur',handleInputText)
 
 function handleInputText(even) {
-    console.log(even.currentTarget.value.length)
-    console.log(inputText.getAttribute('data-length'))
-    if (even.currentTarget.value.length > inputText.getAttribute('data-length')) {
+    if (even.currentTarget.value.length != inputText.getAttribute('data-length')) {
 
         inputText.classList.add('invalid')
         inputText.classList.remove('valid')
