9fd0474ede72ae8e7e0e6ebe6a6a612b655481ee
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-04-19 12:25:54 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,36 @@
 
+const form = document.querySelector('.login-form');
+const formInputs = document.querySelectorAll('input');
+
+form.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const array = [];
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = {
+        Email: mail,
+        Password: password
+    };
+
+    for  (const formInput of formInputs) {
+            if (formInput.value === '') {
+                alert('все поля должны быть заполнены');
+                
+                break;
+            }
+            else  {
+                array.push(formInput);
+            }
+ 
+    }
+     if (array.length === formInputs.length) {
+                console.log(formData);
+     }
+    
+    form.reset()
+    
+    
+}
