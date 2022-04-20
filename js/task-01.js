39ec23f662fd00916ade02f6d38fa95d31abf664
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-04-20 10:08:28 +0300

    task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,12 @@
 const categoryEl = document.querySelectorAll('.item');
-console.log('Number of categories:', categoryEl);
+console.log('Number of categories:', categoryEl.length);
 console.log('Category:', categoryEl[0].firstElementChild.textContent);
 
 const categoryListEl = document.querySelectorAll('.item__list');
-console.log('Elements:', categoryListEl[0].children);
+console.log('Elements:', categoryListEl[0].children.length);
 
 console.log('Category:', categoryEl[1].firstElementChild.textContent);
-console.log('Elements:', categoryListEl[1].children);
+console.log('Elements:', categoryListEl[1].children.length);
 
 console.log('Category:', categoryEl[2].firstElementChild.textContent);
-console.log('Elements:', categoryListEl[2].children);
+console.log('Elements:', categoryListEl[2].children.length);
